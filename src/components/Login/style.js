3b74f1c700c108a8d9039390685d39a253688ee3
shaxboz <shaxa@gmail.com>
2023-08-13 20:34:29 +0500

    test

--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  flex-direction: column
 `;
 
 Wrapper.Container = styled.div`
